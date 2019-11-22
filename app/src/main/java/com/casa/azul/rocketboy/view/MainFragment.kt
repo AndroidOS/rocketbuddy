9e6638caca8d76f7f44c10bd34fd195fad8b3a8b
AndroidOS <burrowsgit@gmail.com>
2019-11-22 21:53:16 +1100

    Tested viewmodel to logcat

--- app/src/main/java/com/casa/azul/rocketboy/view/MainFragment.kt
+++ app/src/main/java/com/casa/azul/rocketboy/view/MainFragment.kt
@@ -7,13 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-
+import androidx.lifecycle.ViewModelProviders
 import com.casa.azul.rocketboy.R
+import com.casa.azul.rocketboy.viewmodel.RocketViewModel
 import kotlinx.android.synthetic.main.fragment_main.*
 
 private const val TAG = "MainFragment"
 
 class MainFragment : Fragment() {
+
+    private lateinit var viewModel: RocketViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +34,14 @@ class MainFragment : Fragment() {
             Navigation.findNavController(it).navigate(action)
         }
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        viewModel = ViewModelProviders.of(this).get(RocketViewModel::class.java)
+        viewModel.getMission()
     }
 
 
