f4a3c1a3b94d35fcc4d472e0774bd4dbe54be7eb
AndroidOS <burrowsgit@gmail.com>
2020-03-30 16:26:57 +1100

    Added PDF method in MainActivity

--- app/src/main/java/com/casa/azul/rocketboy/view/MainActivity.kt
+++ app/src/main/java/com/casa/azul/rocketboy/view/MainActivity.kt
@@ -1,30 +1,53 @@
 package com.casa.azul.rocketboy.view
 
-import android.app.Application
-import android.content.DialogInterface
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Environment
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.casa.azul.rocketboy.R
 import com.casa.azul.rocketboy.util.showAlert
-import com.casa.azul.rocketboy.viewmodel.RocketViewModel
-
+import com.itextpdf.text.Document
+import com.itextpdf.text.Paragraph
+import com.itextpdf.text.pdf.PdfWriter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private val STORAGE_CODE: Int = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            //system OS >= Marshmallow(6.0), check permission is enabled or not
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED
+            ) {
+                //permission was not granted, request it
+                val permissions = arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                requestPermissions(permissions, STORAGE_CODE)
+            }
+
+        }
         navController = Navigation.findNavController(this, R.id.fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
 
@@ -53,5 +76,62 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun savePdf() {
+        Log.d(TAG, "savePDF")
+        //create object of Document class
+        val mDoc = Document()
+        //pdf file name
+        val mFileName = SimpleDateFormat(
+            "yyyyMMdd_HHmmss",
+            Locale.getDefault()
+        ).format(System.currentTimeMillis())
+        //pdf file path
+        val mFilePath =
+            Environment.getExternalStorageDirectory().toString() + "/" + mFileName + ".pdf"
+        try {
+            //create instance of PdfWriter class
+            PdfWriter.getInstance(mDoc, FileOutputStream(mFilePath))
+
+            //open the document for writing
+            mDoc.open()
+
+            //get text from EditText i.e. textEt
+            val mText = "Test string"
+            //add author of the document (metadata)
+            mDoc.addAuthor("Manuel Carvalho")
+
+            //add paragraph to the document
+            mDoc.add(Paragraph(mText))
+
+            //close document
+            mDoc.close()
+
+            //show file saved message with file name and path
+            Toast.makeText(this, "$mFileName.pdf\nis saved to\n$mFilePath", Toast.LENGTH_SHORT)
+                .show()
+        } catch (e: Exception) {
+            //if anything goes wrong causing exception, get and show exception message
+            Log.d(TAG, e.message)
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            STORAGE_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission from popup was granted, call savePdf() method
+                    savePdf()
+                } else {
+                    //permission from popup was denied, show error message
+                    Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 
 }
